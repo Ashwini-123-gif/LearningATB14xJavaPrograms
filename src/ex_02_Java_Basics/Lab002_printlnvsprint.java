4ed2794621a079f42f0e8d763b89446ade6483bb
Ashok819 <ash33058@gmail.com>
2025-10-19 00:25:30 +0530

    intial basic

---
+++ src/ex_02_Java_Basics/Lab002_printlnvsprint.java
@@ -0,0 +1,11 @@
+package ex_02_Java_Basics;
+
+public class Lab002_printlnvsprint {
+    public static void main(String[] args) {
+        System.out.print("Hello");
+        System.out.println("Bye"); // ln -> newline
+
+        System.out.print("Hi ");
+        System.out.print("Bye");
+    }
+}
