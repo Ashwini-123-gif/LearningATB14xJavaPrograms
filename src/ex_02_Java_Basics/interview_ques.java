412079dc423a65843d6fd8879242995a80a52f08
Ashok819 <ash33058@gmail.com>
2025-10-25 23:13:53 +0530

    print ln

---
+++ src/ex_02_Java_Basics/interview_ques.java
@@ -0,0 +1,15 @@
+package ex_02_Java_Basics;
+
+public class interview_ques {
+        public static void main(String[] args) {
+//      int enum = 10;
+          //  int Enum = 10;
+            int pramoddutta  = 89;
+        System.out.println(pramoddutta);
+
+           // int pramod$dutta = 90;
+            //int Lab020_Interview_QnA2 = 123;
+
+        }
+    }
+
