2bdf594b56180c2cd1694caf579b2c5d02a9465c
Ashok819 <ash33058@gmail.com>
2025-10-25 23:27:24 +0530

    constant

---
+++ src/ex_02_Java_Basics/LabConstant.java
@@ -0,0 +1,18 @@
+package ex_02_Java_Basics;
+
+public class LabConstant {
+    public static void main(String[] args) {
+       // int a = 10;
+       // a = 11;
+        final int b = 99;
+        //b = 100;
+        System.out.println(b);
+
+       // final int b = 99;
+        // b = 100; // java: cannot assign a value to final variable b
+
+      //  final float PI = 3.14f;
+        //final int a1 = 10;
+
+    }
+}
